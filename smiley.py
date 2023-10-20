2135b3d449bf1d6b771309cd672184f84ab5b269
Ella Rodger <ellarodger@outlook.com.au>
2023-10-20 14:19:41 +0800

    Updating for flexible smiley complexion

--- smiley.py
+++ smiley.py
@@ -7,12 +7,18 @@ class Smiley:
     RED = (255, 0, 0)
     YELLOW = (255, 255, 0)
     BLANK = (0, 0, 0)
+    BLUE = (0, 0, 255)
 
-    def __init__(self):
+    
+    
+
+    def __init__(self, complexion = YELLOW):
+        
         # We have encapsulated the SenseHat object
         self.sense_hat = SenseHat()
+        self.my_complexion = complexion
 
-        Y = self.YELLOW
+        Y = complexion
         O = self.BLANK
         self.pixels = [
             O, Y, Y, Y, Y, Y, Y, O,
@@ -25,6 +31,9 @@ class Smiley:
             O, Y, Y, Y, Y, Y, Y, O,
         ]
 
+    def complexion(self):
+        return self.my_complexion
+        
     def dim_display(self, dimmed=True):
         """
         Set the SenseHat's light intensity to low (True) or high (False)
@@ -37,3 +46,7 @@ class Smiley:
         Show the smiley on the screen.
         """
         self.sense_hat.set_pixels(self.pixels)
+
+    
+
+    
